47ee5a21349ced8979b5ad0ee77f733db14efd7f
Dáire McDermott <dairemcdermott@outlook.com>
2023-03-21 22:18:10 +0000

    Initial Commit

---
+++ source/org/jfree/data/DataUtilitiesTest.java
@@ -0,0 +1,466 @@
+package org.jfree.data;
+
+import static org.junit.Assert.*;
+
+import org.jfree.data.DataUtilities;
+import org.jfree.data.DefaultKeyedValues;
+import org.jfree.data.DefaultKeyedValues2D;
+import org.jfree.data.KeyedValues;
+import org.jfree.data.Values2D;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import org.jfree.data.category.DefaultCategoryDataset;
+
+public class DataUtilitiesTest extends DataUtilities 
+{
+	private Values2D values2D;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception
+	{
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception
+	{
+	}
+
+	@Before
+	public void setUp() throws Exception 
+	{
+		DefaultKeyedValues2D testValues = new DefaultKeyedValues2D();
+		values2D = testValues;
+		testValues.addValue(1, 0, 0);
+		testValues.addValue(4, 1, 0);
+	}
+
+	@After
+	public void tearDown() throws Exception 
+	{
+		values2D = null;
+	}
+
+	@Test
+	public void testValidDataAndColumnColumnTotal() 
+	{
+		assertEquals("Wrong sum returned. It should be 5.0",
+						5.0, DataUtilities.calculateColumnTotal(values2D,  0), 0.0000001d);
+	}
+	
+	@Test // getCumlativePrecentages Function
+	public void testgetCumlativePercenetages() 
+	{
+		// setup
+		
+		DefaultKeyedValues keyvalues = new DefaultKeyedValues();
+		keyvalues.addValue((Comparable) 0.0, 6.0);
+		keyvalues.addValue((Comparable) 1.0, 11.0);
+		keyvalues.addValue((Comparable) 2.0, 3.0);
+		KeyedValues object_under_test = DataUtilities.getCumulativePercentages((KeyedValues) keyvalues);
+		
+		assertEquals((double) object_under_test.getValue(2), 1.0, .000000001d);
+	}
+	
+	@Test // calculateColumnTotal function
+	public void testNullDataColumnTotal()
+	{
+		try
+		{
+			DataUtilities.calculateColumnTotal(null,  0);
+			fail("No exception thrown. The expected outcome was: a thrown exception of type: IllegalArgumentException");
+					 			
+		}
+		catch (Exception e)
+		{
+			assertTrue("Incorrest exception type thrown",
+					e.getClass().equals(IllegalArgumentException.class));
+		}
+	}
+	
+	@Test // calculateColumnTotal function
+	public void testZeroBasedColumnIndex()
+	{
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		dataset.addValue(1, "Row 1", "Column 1");
+		dataset.addValue(2, "Row 1", "Column 2");
+		dataset.addValue(3, "Row 1", "Column 3");
+		dataset.addValue(5, "Row 2", "Column 1");
+		dataset.addValue(6, "Row 2", "Column 2");
+		dataset.addValue(7, "Row 2", "Column 3");
+		dataset.addValue(8, "Row 3", "Column 1");
+		dataset.addValue(5, "Row 3", "Column 2");
+		dataset.addValue(3, "Row 3", "Column 3");
+		int column = 0;
+		double expectedTotal = 14.0;
+		double delta = 0.0001;
+		
+		double actualTotal = DataUtilities.calculateColumnTotal(dataset, column);
+		
+		assertEquals(expectedTotal, actualTotal, delta);
+	}
+	
+	@Test // calculateColumnTotal function 
+	public void testOutOfBoundsColumnIndex()
+	{
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		dataset.addValue(1, "Row 1", "Column 1");
+		dataset.addValue(2, "Row 1", "Column 2");
+		dataset.addValue(3, "Row 1", "Column 3");
+		dataset.addValue(5, "Row 2", "Column 1");
+		dataset.addValue(6, "Row 2", "Column 2");
+		dataset.addValue(7, "Row 2", "Column 3");
+		dataset.addValue(8, "Row 3", "Column 1");
+		dataset.addValue(5, "Row 3", "Column 2");
+		dataset.addValue(3, "Row 3", "Column 3");
+		int columnIndex = 4;
+		
+		try
+		{
+	        DataUtilities.calculateColumnTotal(dataset, columnIndex);
+	        fail("Expected IndexOutOfBoundsException was not thrown");
+	    }
+		catch (IndexOutOfBoundsException e)
+		{
+	        
+	    }
+	}
+	
+	@Test // calculateColumnTotal function
+	public void testValidInput()
+	{
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		dataset.addValue(1, "Row 1", "Column 1");
+		dataset.addValue(2, "Row 1", "Column 2");
+		dataset.addValue(3, "Row 1", "Column 3");
+		dataset.addValue(5, "Row 2", "Column 1");
+		dataset.addValue(6, "Row 2", "Column 2");
+		dataset.addValue(7, "Row 2", "Column 3");
+		dataset.addValue(8, "Row 3", "Column 1");
+		dataset.addValue(5, "Row 3", "Column 2");
+		dataset.addValue(3, "Row 3", "Column 3");
+		int column = 2;
+		double expectedTotal = 13.0;
+		double delta = 0.0001;
+		
+		double actualTotal = DataUtilities.calculateColumnTotal(dataset, column);
+		
+		assertEquals(expectedTotal, actualTotal, delta);
+	}
+	
+	@Test // calculateColumnTotal function 
+	public void testOutOfBoundsColumnNegativeIndex()
+	{
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		dataset.addValue(1, "Row 1", "Column 1");
+		dataset.addValue(2, "Row 1", "Column 2");
+		dataset.addValue(3, "Row 1", "Column 3");
+		dataset.addValue(5, "Row 2", "Column 1");
+		dataset.addValue(6, "Row 2", "Column 2");
+		dataset.addValue(7, "Row 2", "Column 3");
+		dataset.addValue(8, "Row 3", "Column 1");
+		dataset.addValue(5, "Row 3", "Column 2");
+		dataset.addValue(3, "Row 3", "Column 3");
+		int columnIndex = -3;
+		
+		try
+		{
+	        DataUtilities.calculateColumnTotal(dataset, columnIndex);
+	        fail("Expected IndexOutOfBoundsException was not thrown");
+	    }
+		catch (IndexOutOfBoundsException e)
+		{
+	        
+	    }
+	}
+	
+	@Test // calculateColumnTotal function
+	public void testTableWithNegatives()
+	{
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		dataset.addValue(1, "Row 1", "Column 1");
+		dataset.addValue(2, "Row 1", "Column 2");
+		dataset.addValue(3, "Row 1", "Column 3");
+		dataset.addValue(5, "Row 2", "Column 1");
+		dataset.addValue(-6, "Row 2", "Column 2");
+		dataset.addValue(-7, "Row 2", "Column 3");
+		dataset.addValue(8, "Row 3", "Column 1");
+		dataset.addValue(5, "Row 3", "Column 2");
+		dataset.addValue(3, "Row 3", "Column 3");
+		int column = 1;
+		double expectedTotal = 1.0;
+		double delta = 0.0001;
+		
+		double actualTotal = DataUtilities.calculateColumnTotal(dataset, column);
+		
+		assertEquals(expectedTotal, actualTotal, delta);
+	}
+	
+	@Test // calculateRowTotal
+	public void testNullDataRowTotal()
+	{
+		try
+		{
+			DataUtilities.calculateRowTotal(null,  0);
+			fail("No exception thrown. The expected outcome was: a thrown exception of type: IllegalArgumentException");
+					 			
+		}
+		catch (Exception e)
+		{
+			assertTrue("Incorrest exception type thrown",
+					e.getClass().equals(IllegalArgumentException.class));
+		}
+	}
+	
+	@Test // calculateRowTotal function
+	public void testZeroBasedRowIndex()
+	{
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		dataset.addValue(1, "Row 1", "Column 1");
+		dataset.addValue(2, "Row 1", "Column 2");
+		dataset.addValue(3, "Row 1", "Column 3");
+		dataset.addValue(5, "Row 2", "Column 1");
+		dataset.addValue(6, "Row 2", "Column 2");
+		dataset.addValue(7, "Row 2", "Column 3");
+		dataset.addValue(8, "Row 3", "Column 1");
+		dataset.addValue(5, "Row 3", "Column 2");
+		dataset.addValue(3, "Row 3", "Column 3");
+		int row = 0;
+		double expectedTotal = 6.0;
+		double delta = 0.0001;
+		
+		double actualTotal = DataUtilities.calculateRowTotal(dataset, row);
+		
+		assertEquals(expectedTotal, actualTotal, delta);
+	}
+	
+	@Test // calculateRowTotal function
+	public void testOutOfBoundsRowIndex()
+	{
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		dataset.addValue(1, "Row 1", "Column 1");
+		dataset.addValue(2, "Row 1", "Column 2");
+		dataset.addValue(3, "Row 1", "Column 3");
+		dataset.addValue(5, "Row 2", "Column 1");
+		dataset.addValue(6, "Row 2", "Column 2");
+		dataset.addValue(7, "Row 2", "Column 3");
+		dataset.addValue(8, "Row 3", "Column 1");
+		dataset.addValue(5, "Row 3", "Column 2");
+		dataset.addValue(3, "Row 3", "Column 3");
+		int rowIndex = 3;
+		
+		try
+		{
+	        DataUtilities.calculateRowTotal(dataset, rowIndex);
+	        fail("Expected IndexOutOfBoundsException was not thrown");
+	    }
+		catch (IndexOutOfBoundsException e)
+		{
+	        
+	    }
+	}
+	
+	@Test // calculateRowTotal function
+	public void testValidRowInput()
+	{
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		dataset.addValue(1, "Row 1", "Column 1");
+		dataset.addValue(2, "Row 1", "Column 2");
+		dataset.addValue(3, "Row 1", "Column 3");
+		dataset.addValue(5, "Row 2", "Column 1");
+		dataset.addValue(6, "Row 2", "Column 2");
+		dataset.addValue(7, "Row 2", "Column 3");
+		dataset.addValue(8, "Row 3", "Column 1");
+		dataset.addValue(5, "Row 3", "Column 2");
+		dataset.addValue(3, "Row 3", "Column 3");
+		int rowIndex = 2;
+		double expectedTotal = 16.0;
+		double delta = 0.0001;
+		
+		double actualTotal = DataUtilities.calculateRowTotal(dataset, rowIndex);
+		
+		assertEquals(expectedTotal, actualTotal, delta);
+	}
+	
+	@Test // calculateRowTotal function 
+	public void testOutOfBoundsRowNegativeIndex()
+	{
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		dataset.addValue(1, "Row 1", "Column 1");
+		dataset.addValue(2, "Row 1", "Column 2");
+		dataset.addValue(3, "Row 1", "Column 3");
+		dataset.addValue(5, "Row 2", "Column 1");
+		dataset.addValue(6, "Row 2", "Column 2");
+		dataset.addValue(7, "Row 2", "Column 3");
+		dataset.addValue(8, "Row 3", "Column 1");
+		dataset.addValue(5, "Row 3", "Column 2");
+		dataset.addValue(3, "Row 3", "Column 3");
+		int rowIndex = -3;
+		
+		try
+		{
+	        DataUtilities.calculateRowTotal(dataset, rowIndex);
+	        fail("Expected IndexOutOfBoundsException was not thrown");
+	    }
+		catch (IndexOutOfBoundsException e)
+		{
+	        
+	    }
+	}
+	
+	@Test // calculateColumnTotal function
+	public void testTableWithNegativesRow()
+	{
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		dataset.addValue(1, "Row 1", "Column 1");
+		dataset.addValue(2, "Row 1", "Column 2");
+		dataset.addValue(3, "Row 1", "Column 3");
+		dataset.addValue(5, "Row 2", "Column 1");
+		dataset.addValue(-6, "Row 2", "Column 2");
+		dataset.addValue(-7, "Row 2", "Column 3");
+		dataset.addValue(8, "Row 3", "Column 1");
+		dataset.addValue(5, "Row 3", "Column 2");
+		dataset.addValue(3, "Row 3", "Column 3");
+		int rowIndex = 1;
+		double expectedTotal = -8.0;
+		double delta = 0.0001;
+		
+		double actualTotal = DataUtilities.calculateRowTotal(dataset, rowIndex);
+		
+		assertEquals(expectedTotal, actualTotal, delta);
+	}
+	
+	@Test // createNumberArray 
+	public void testNullcreateNumberArray()
+	{
+		try
+		{
+			DataUtilities.createNumberArray(null);
+			fail("No exception thrown. The expected outcome was: a thrown exception of type: IllegalArgumentException");
+					 			
+		}
+		catch (Exception e)
+		{
+			assertTrue("Incorrest exception type thrown",
+					e.getClass().equals(IllegalArgumentException.class));
+		}
+	}
+	
+	@Test // createNumberArray
+	public void testValidCreateNumberArray()
+	{
+		double[] data = {1.0, 2.0, 3.4, 5.6};
+		Number[] expected = {1.0, 2.0, 3.4, 5.6};
+	    Number[] actual = DataUtilities.createNumberArray(data);
+	    assertArrayEquals(expected, actual);
+	}
+	
+	@Test // createNumberArray
+	public void testLetterInputCreateNumberArray()
+	{
+		double[] data = {'a','b','c','d'};
+		
+		try
+		{
+			DataUtilities.createNumberArray(data);
+			fail("No exception thrown. The expected outcome was: a thrown exception of type: IllegalArgumentException");
+					 			
+		}
+		catch (Exception e)
+		{
+			assertTrue("Incorrest exception type thrown",
+					e.getClass().equals(IllegalArgumentException.class));
+		}
+	}
+	
+	@Test // createNumberArray2D 
+	public void testNullcreateNumberArray2D()
+	{
+		try
+		{
+			DataUtilities.createNumberArray2D(null);
+			fail("No exception thrown. The expected outcome was: a thrown exception of type: IllegalArgumentException");
+					 			
+		}
+		catch (Exception e)
+		{
+			assertTrue("Incorrest exception type thrown",
+					e.getClass().equals(IllegalArgumentException.class));
+		}
+	}
+	
+	@Test // createNumberArray2D
+	public void testValidCreateNumberArray2d()
+	{
+		double[][] data = {{1.0, 2.0}, {3.4, 5.6}, {7.8,9.6}};
+		Number[][] expected = {{1.0, 2.0}, {3.4, 5.6}, {7.8,9.6}};
+	    Number[][] actual = DataUtilities.createNumberArray2D(data);
+	    assertArrayEquals(expected, actual);
+	}
+	
+	@Test // createNumberArray2D
+	public void testLetterInputCreateNumberArray2D()
+	{
+		double[][] data = {{'a','b'},{'c','d'}};
+		
+		try
+		{
+			DataUtilities.createNumberArray2D(data);
+			fail("No exception thrown. The expected outcome was: a thrown exception of type: IllegalArgumentException");
+					 			
+		}
+		catch (Exception e)
+		{
+			assertTrue("Incorrest exception type thrown",
+					e.getClass().equals(IllegalArgumentException.class));
+		}
+	}
+	
+	@Test // getCumulativePercentages 
+	public void testNullgetCumulativePercentages()
+	{
+		try
+		{
+			DataUtilities.getCumulativePercentages(null);
+			fail("No exception thrown. The expected outcome was: a thrown exception of type: IllegalArgumentException");
+					 			
+		}
+		catch (Exception e)
+		{
+			assertTrue("Incorrest exception type thrown",
+					e.getClass().equals(IllegalArgumentException.class));
+		}
+	}
+
+	@Test // getCumlativePercentages()
+	public void testValidGetCumulativePercentage()
+	{
+		KeyedValues values = new DefaultKeyedValues();
+	    ((DefaultKeyedValues) values).addValue("A", 1);
+	    ((DefaultKeyedValues) values).addValue("B", 2);
+	    ((DefaultKeyedValues) values).addValue("C", 3);
+	    ((DefaultKeyedValues) values).addValue("D", 4);
+	    ((DefaultKeyedValues) values).addValue("E", 5);
+	    
+	    KeyedValues expected = new DefaultKeyedValues();
+	    ((DefaultKeyedValues) expected).addValue("A", 0.2);
+	    ((DefaultKeyedValues) expected).addValue("B", 0.4);
+	    ((DefaultKeyedValues) expected).addValue("C", 0.6);
+	    ((DefaultKeyedValues) expected).addValue("D", 0.8);
+	    ((DefaultKeyedValues) expected).addValue("E", 1.0);
+	    
+	    
+	    KeyedValues actual = DataUtilities.getCumulativePercentages(values);
+	    
+	    for (int i = 0; i < expected.getItemCount(); i++)
+	    {
+	        Comparable key = expected.getKey(i);
+	        double expectedValue = expected.getValue(key).doubleValue();
+	        double actualValue = actual.getValue(key).doubleValue();
+	        assertEquals(expectedValue, actualValue, 0.0001);
+	    }
+	}	
+}
---
+++ source/org/jfree/data/RangeTest.java
@@ -0,0 +1,364 @@
+package org.jfree.data;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.jfree.data.Range;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RangeTest
+{
+	private Range rangeObjectUnderTest;
+	private Range rangeObjectUnderTest2;
+
+	@Before
+	public void setUp() throws Exception
+	{
+		rangeObjectUnderTest = new Range(-1, 1);
+		rangeObjectUnderTest2 = new Range(4, 9);
+	}
+
+	@After
+	public void tearDown() throws Exception
+	{
+	}
+	
+	// EXAMPLE TESTS
+	
+	@Test
+	public void testCentralValueShouldBeZero()
+	{
+		assertEquals("The central value of -1 and 1 should be 0",
+				0,
+				rangeObjectUnderTest.getCentralValue(),
+				0.000000001d);
+	}
+	
+	@Test
+	public void testGetLengthBothPositiveAndEqual()
+	{
+		Range r1 = new Range(2, 2);
+		assertEquals("getLength: Did not return the expected output",
+				0.0,
+				r1.getLength(),
+				0.000000001d);
+	}
+	
+	@Test
+	public void testGetLengthBothPositiveAndNotEqual()
+	{
+		Range r2 = new Range(4, 9);
+		assertEquals("getLength: Did not return the expected output",
+				5.0,
+				r2.getLength(),
+				0.000000001d);
+	}
+	
+	@Test
+	public void testGetLengthBothNegativeAndEqual()
+	{
+		Range r3 = new Range(-99, -99);
+		assertEquals("getLength: Did not return the expected output",
+				0.0,
+				r3.getLength(),
+				0.000000001d);
+	}
+	
+	@Test
+	public void testGetLengthBothNegativeAndNotEqual()
+	{
+		Range r4 = new Range(-11, -4);
+		assertEquals("getLength: Did not return the expected output",
+				7.0,
+				r4.getLength(),
+				0.000000001d);
+	}
+	
+	@Test
+	public void testGetLengthOnePositiveOneNegative()
+	{
+		Range r5 = new Range(-5, 3);
+		assertEquals("getLength: Did not return the expected output",
+				8.0,
+				r5.getLength(),
+				0.000000001d);
+	}
+	
+	// ADDED TESTS
+	
+	// Method Under Test: intersects
+	
+	@Test
+	public void testIntersectsLowerAndUpperIsSameAsObject()
+	{
+		assertEquals("intersects: Did not return the expected output",
+				true,
+				rangeObjectUnderTest2.intersects(4, 9));
+	}
+	
+	@Test
+	public void testIntersectsLowerIsJustBelowLowerBound()
+	{
+		assertEquals("intersects: Did not return the expected output",
+				true,
+				rangeObjectUnderTest2.intersects(3, 4));
+	}
+	
+	@Test
+	public void testIntersectsUpperIsJustAboveUpperBound()
+	{
+		assertEquals("intersects: Did not return the expected output",
+				true,
+				rangeObjectUnderTest2.intersects(9, 10));
+	}
+	
+	@Test
+	public void testIntersectsSmallestPossibleRange()
+	{
+		assertEquals("intersects: Did not return the expected output",
+				false,
+				rangeObjectUnderTest2.intersects(0, 0));
+	}
+	
+	@Test
+	public void testIntersectsLargestPossibleRange()
+	{
+		assertEquals("intersects: Did not return the expected output",
+				true,
+				rangeObjectUnderTest2.intersects(Double.MIN_VALUE,
+						Double.MAX_VALUE));
+	}
+	
+	// Method Under Test: constrain
+	
+	@Test
+	public void testConstrainValueEqualToLowerBound()
+	{
+		assertEquals("contrain: Did not return the expected output",
+				4,
+				rangeObjectUnderTest2.constrain(4),
+				0.000000001d);
+	}
+	
+	@Test
+	public void testConstrainValueEqualToUpperBound()
+	{
+		assertEquals("contrain: Did not return the expected output",
+				9,
+				rangeObjectUnderTest2.constrain(9),
+				0.000000001d);
+	}
+	
+	@Test
+	public void testConstrainValueJustBelowLowerBound()
+	{
+		assertEquals("contrain: Did not return the expected output",
+				4,
+				rangeObjectUnderTest2.constrain(3),
+				0.000000001d);
+	}
+	
+	@Test
+	public void testConstrainValueJustAboveUpperBound()
+	{
+		assertEquals("contrain: Did not return the expected output",
+				9,
+				rangeObjectUnderTest2.constrain(10),
+				0.000000001d);
+	}
+	
+	@Test
+	public void testConstrainValueFarBelowLowerBound()
+	{
+		assertEquals("contrain: Did not return the expected output",
+				4,
+				rangeObjectUnderTest2.constrain(-99),
+				0.000000001d);
+	}
+	
+	@Test
+	public void testConstrainValueFarAboveUpperBound()
+	{
+		assertEquals("contrain: Did not return the expected output",
+				9,
+				rangeObjectUnderTest2.constrain(99),
+				0.000000001d);
+	}
+	
+	// Method Under Test: contains
+	
+	@Test
+	public void testContainsValueEqualToLowerBound()
+	{
+		assertTrue("contains: Did not return the expected output",
+				rangeObjectUnderTest2.contains(4));
+	}
+	
+	@Test
+	public void testContainsValueEqualToUpperBound()
+	{
+		assertTrue("contains: Did not return the expected output",
+				rangeObjectUnderTest2.contains(9));
+	}
+	
+	@Test
+	public void testContainsValueJustBelowLowerBound()
+	{
+		assertFalse("contains: Did not return the expected output",
+				rangeObjectUnderTest2.contains(3));
+	}
+	
+	@Test
+	public void testContainsValueJustAboveUpperBound()
+	{
+		assertFalse("contains: Did not return the expected output",
+				rangeObjectUnderTest2.contains(10));
+	}
+	
+	@Test
+	public void testContainsValueFarBelowLowerBound()
+	{
+		assertFalse("contains: Did not return the expected output",
+				rangeObjectUnderTest2.contains(-99));
+	}
+	
+	@Test
+	public void testContainsValueFarAboveUpperBound()
+	{
+		assertFalse("contains: Did not return the expected output",
+				rangeObjectUnderTest2.contains(99));
+	}
+	
+	// Method Under Test: getCentralValue
+	
+	@Test
+	public void testGetCentralValueRangeHasOneValue()
+	{
+		Range r6 = new Range(2, 2);
+		
+		assertEquals("getCentralValue: Did not return the expected output",
+				2,
+				r6.getCentralValue(),
+				0.000000001d);
+	}
+	
+	@Test
+	public void testGetCentralValueRangeHasTwoValues()
+	{
+		Range r7 = new Range(2, 3);
+		
+		assertEquals("getCentralValue: Did not return the expected output",
+				2.5,
+				r7.getCentralValue(),
+				0.000000001d);
+	}
+	
+	@Test
+	public void testGetCentralValueRangeHasThreeValues()
+	{
+		Range r8 = new Range(2, 4);
+		
+		assertEquals("getCentralValue: Did not return the expected output",
+				3,
+				r8.getCentralValue(),
+				0.000000001d);
+	}
+	
+	@Test
+	public void testGetCentralValueRangeHasFourValues()
+	{
+		Range r9 = new Range(2, 5);
+		
+		assertEquals("getCentralValue: Did not return the expected output",
+				3.5,
+				r9.getCentralValue(),
+				0.000000001d);
+	}
+	
+	// Method Under Test: expandToInclude
+	
+	@Test
+	public void testExpandToIncludeRangeIsNull()
+	{
+		Range r10 = null;
+		
+		assertEquals("expandToInclude: Did not return the expected output",
+				new Range(5, 5),
+				Range.expandToInclude(r10, 5));
+	}
+	
+	@Test
+	public void testExpandToIncludeRangeHasOneValue()
+	{
+		Range r11 = new Range(4, 4);
+		
+		assertEquals("expandToInclude: Did not return the expected output",
+				new Range(3, 4),
+				Range.expandToInclude(r11, 3));
+	}
+	
+	@Test
+	public void testExpandToIncludeRangeHasTwoValuesAndInputLessThanLowerBound()
+	{
+		Range r12 = new Range(4, 5);
+		
+		assertEquals("expandToInclude: Did not return the expected output",
+				new Range(3, 5),
+				Range.expandToInclude(r12, 3));
+	}
+	
+	@Test
+	public void testExpandToIncludeRangeHasTwoValuesAndInputWithinBounds()
+	{
+		Range r13 = new Range(4, 5);
+		
+		assertEquals("expandToInclude: Did not return the expected output",
+				new Range(4, 5),
+				Range.expandToInclude(r13, 4.5));
+	}
+	
+	@Test
+	public void testExpandToIncludeRangeHasTwoValuesAndInputMoreThanUpperBound()
+	{
+		Range r14 = new Range(4, 5);
+		
+		assertEquals("expandToInclude: Did not return the expected output",
+				new Range(4, 6),
+				Range.expandToInclude(r14, 6));
+	}
+	
+	@Test
+	public void testExpandToIncludeRangeHasMoreThanTwoValuesAndInputLessThanLowerBound()
+	{
+		Range r15 = new Range(3, 6);
+		
+		assertEquals("expandToInclude: Did not return the expected output",
+				new Range(1, 6),
+				Range.expandToInclude(r15, 1));
+	}
+	
+	@Test
+	public void testExpandToIncludeRangeHasMoreThanTwoValuesAndInputWithinBounds()
+	{
+		Range r16 = new Range(3, 6);
+		
+		assertEquals("expandToInclude: Did not return the expected output",
+				new Range(3, 6),
+				Range.expandToInclude(r16, 5));
+	}
+	
+	@Test
+	public void testExpandToIncludeRangeHasMoreThanTwoValuesAndInputMoreThanUpperBound()
+	{
+		Range r17 = new Range(3, 6);
+		
+		assertEquals("expandToInclude: Did not return the expected output",
+				new Range(3, 8),
+				Range.expandToInclude(r17, 8));
+	}
+}
